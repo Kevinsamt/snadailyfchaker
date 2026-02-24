b68bd9c2e2a0341ea8df7ab957e78676ec377873
Kevinsamt <Kevinsamuelyt04gmai.com>
2026-02-24 13:07:41 +0700

    Debug: Add standalone test-function

---
+++ api/test-function.js
@@ -0,0 +1,8 @@
+export default function (req, res) {
+    res.json({
+        success: true,
+        message: "Standalone Vercel Function reached!",
+        url: req.url,
+        query: req.query
+    });
+}
