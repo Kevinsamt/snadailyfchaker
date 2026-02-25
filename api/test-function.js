f7d70eb5f0fca12dcf7670e1117de79f7c11fd52
Kevinsamt <Kevinsamuelyt04gmai.com>
2026-02-25 12:04:45 +0700

    fix: hapus folder api/ yang konflik dengan server.js di vercel.json

--- api/test-function.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (req, res) {
-    res.json({
-        success: true,
-        message: "Standalone Vercel Function reached!",
-        url: req.url,
-        query: req.query
-    });
-};
