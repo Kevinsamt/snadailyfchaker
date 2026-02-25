686f0bd0c57722ba86c55fd8d559ec4a697c233c
Kevinsamt <Kevinsamuelyt04gmai.com>
2026-02-25 12:00:39 +0700

    fix: ganti ES module syntax ke CommonJS di api/test-function.js

--- api/test-function.js
+++ api/test-function.js
@@ -1,8 +1,8 @@
-export default function (req, res) {
+module.exports = function (req, res) {
     res.json({
         success: true,
         message: "Standalone Vercel Function reached!",
         url: req.url,
         query: req.query
     });
-}
+};
